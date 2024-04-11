ad35e71144bbc72fe98e3949a2cd7871cc0fdce9
filiprendon <frendont2223@politecnics.barcelona>
2024-04-11 15:34:06 +0200

    eventHandler

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let boton = document.querySelector(".btn");
+// let boton = document.querySelector(".btn");
 
-boton.addEventListener("click", handleEvent);
+// boton.addEventListener("click", handleEvent);
 
-function handleEvent(event) {
-  switch (event.type) {
-    case "click":
-      saludo();
-      pregunta();
-      break;
-  }
+// function handleEvent(event) {
+//   switch (event.type) {
+//     case "click":
+//       saludo();
+//       pregunta();
+//       break;
+//   }
+// }
+
+// function saludo() {
+//   alert("Hola");
+// }
+
+// function pregunta() {
+//   confirm("Seguro que quieres continuar?");
+// }
+
+
+const imagen = document.getElementById('imagen');
+const boton = document.getElementById('button');
+
+function hanHechoClick(event){
+    if(event.target === imagen){
+        alert('Has hecho click a la imagen');
+    }
+    if(event.target === boton){
+        alert('Has hecho click al botón');
+    }
 }
 
-function saludo() {
-  alert("Hola");
+function mensajeHover(event){
+    if(event.target === imagen){
+        document.title = 'A380';
+    }
+    if(event.target === boton){
+        document.title = 'Es botón';
+    }
 }
-
-function pregunta() {
-  confirm("Seguro que quieres continuar?");
-}
+imagen.addEventListener("click", hanHechoClick);
+boton.addEventListener("click", hanHechoClick);
+imagen.addEventListener('mouseover', mensajeHover);
+boton.addEventListener('mouseover', mensajeHover);
