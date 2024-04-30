e8cff9fa47e54f0f5e882267dffed76e7f322a87
filiprendon <frendont2223@politecnics.barcelona>
2024-04-30 17:48:31 +0200

    .

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let boton = document.querySelector(".btn");
 
+let numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 boton.addEventListener("click", handleEvent);
+boton.addEventListener("contextmenu", handleEvent);
+boton.addEventListener("mouseover", handleEvent);
+boton.addEventListener("mouseout", handleEvent);
+document.addEventListener("mousemove", handleEvent);
 
 function handleEvent(event) {
   switch (event.type) {
     case "click":
       saludo();
       pregunta();
+      break;
+
+    case "contextmenu":
+      contextFunc();
+      break;
+    case "mouseover":
+      miFunc();
+      break;
+    case "mouseout":
+      moFunc();
+      break;
+    case "mousemove":
+      moveFunc();
       break;
   }
 }
@@ -17,4 +35,25 @@ function saludo() {
 
 function pregunta() {
   confirm("Seguro que quieres continuar?");
+}
+
+function contextFunc() {
+  alert("Has hecho click derecho");
+}
+
+function miFunc() {
+  document.body.style.backgroundColor = "green";
+}
+
+function moFunc() {
+  document.body.style.backgroundColor = "#333";
+}
+
+function moveFunc() {
+  boton.textContent = generarNumeroAleatorio();
+}
+
+function generarNumeroAleatorio() {
+  let randomNum = Math.floor(Math.random() * numeros.length);
+  return numeros[randomNum];
 }
